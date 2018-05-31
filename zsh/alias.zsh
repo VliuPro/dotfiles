f2ebdc593d3cae0333c3473b54e63461378a64e4
urzz <virus_liu@163.com>
2018-05-31 16:33:09 +0800

    update zsh config

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -9,11 +9,5 @@ if which vim &>/dev/null; then
     alias vi='vim'
 fi
 
-alias harbor='docker run \
- -e HARBOR_USERNAME="admin" \
- -e HARBOR_PASSWORD="Harbor12345" \
- -e HARBOR_URL="http://192.168.56.4" \
- --net host --rm krystism/harborclient'
 alias ip='docker run --privileged -t -i --rm --network=host alpine ip'
 
-alias j='z'
